6fc0ff34c4c1fa501d02001e09d6fd28dabc7629
Kevin Machava <kevinmachava20@gmail.com>
2023-02-18 00:16:14 +0200

    another one

--- script.js
+++ script.js
@@ -1,79 +1,95 @@
 import bot from './assets/bot.svg'
 import user from './assets/user.svg'
+
 const form = document.querySelector('form')
 const chatContainer = document.querySelector('#chat_container')
 
 let loadInterval
 
-// Loader while await the response
 function loader(element) {
-  element.textContent = ''
-  loadInterval = setInterval(()=>{
-    element.textContent += '.'
+    element.textContent = ''
 
-    if(element.textContent === '....'){
-      element.textContent= ''
-    }
-  },300)
+    loadInterval = setInterval(() => {
+        // Update the text content of the loading indicator
+        element.textContent += '.';
+
+        // If the loading indicator has reached three dots, reset it
+        if (element.textContent === '....') {
+            element.textContent = '';
+        }
+    }, 300);
 }
-// Type text letter by letter
-function typeText(element, text){
-  let index = 0
-  let interval = setInterval(()=>{
-    if(index < text.length){
-      element.innerHTML += text.charAt(index)
-      index++
-    }else{
-      clearInterval(interval)
-    }
-  },20)
 
-  function generateUniqueId(){
-    const timestamp = Date.now()
-    const randomNumber = Math.random()
-    const hexadecimalString = randomNumber.toString(16)
-    return `id-${timestamp}-${hexadecimalString}`
-  }
+function typeText(element, text) {
+    let index = 0
 
-  function chatStripe(isAi, value, uniqueId){
-    return(
-      `
-      <div class= "wrapper ${isAi && 'ai'}">
-        <div class="chat">
-          <div class="profile">
-            <img src="${isAi? bot: user}" alt="${isAi? 'bot': 'user'}"/>
-          </div>
-          <div class="message" id=${uniqueId}> ${value}</div>
+    let interval = setInterval(() => {
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index)
+            index++
+        } else {
+            clearInterval(interval)
+        }
+    }, 20)
+}
+
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
+function generateUniqueId() {
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueId) {
+    return (
+        `
+        <div class="wrapper ${isAi && 'ai'}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? 'bot' : 'user'}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
         </div>
-      </div>
-      `
+    `
     )
-  }
+}
 
-  const handleSubmit = async(e) =>{
-    e.preventDefault();
+const handleSubmit = async (e) => {
+    e.preventDefault()
+
     const data = new FormData(form)
-   
-    //user chatStripe
-    chatContainer.innerHTML+= chatStripe(false, data.get('prompt'))
+
+    // user's chatstripe
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+
+    // to clear the textarea input 
     form.reset()
 
-    //bot chatStripe
+    // bot's chatstripe
     const uniqueId = generateUniqueId()
-    chatContainer.innerHTML+= chatStripe(true, " ", uniqueId)
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
 
-    //scroll the view while the bot's typing
-    chatContainer.scrollTop = chatContainer.scrollHeight
+    // to focus scroll to the bottom 
+    chatContainer.scrollTop = chatContainer.scrollHeight;
 
+    // specific message div 
     const messageDiv = document.getElementById(uniqueId)
 
+    // messageDiv.innerHTML = "..."
     loader(messageDiv)
-  }
-  form.addEventListener('submit', handleSubmit)
-  //submit using enter button
-  form.addEventListener('keyup', (e)=>{
-    if (e.keyCode===13) {
-      handleSubmit(e)
-    }
-  })
 }
+
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        handleSubmit(e)
+    }
+})
