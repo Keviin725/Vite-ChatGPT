8775a0fa67c4bd449e2ede1f139accbf3a0b096e
Kevin Machava <kevinmachava20@gmail.com>
2023-02-18 02:08:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv)
     
     // fetch data from server: bot's response
-    const response = await fetch('http://localhost:3000', {
+    const response = await fetch('https://chatgptserver-wy4f.onrender.com/', {
       method: 'POST',
       headers:{
         'Content-Type': 'application/json'
